9d023f3f6fece31ba5db50885114fc1598aa0a7f
Anton Brechka <a.brechka@g.nsu.ru>
2019-02-05 23:51:52 +0700

    java projects

---
+++ java/wordCount/src/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[]args) throws IOException{
+        Table CSV=new Table();
+        CSV.execute("/home/anton/Documents/dataForPrograms/java/input.txt");
+    }
+}
+
